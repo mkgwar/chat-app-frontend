9753938c8a48621f6b4140855d4dd8cbf3fef240
mkgwar <mohitgangwarknp@gmail.com>
2022-02-08 18:36:53 +0530

    v1.2 rooms feature added

--- src/Components/LeftPanel.js
+++ src/Components/LeftPanel.js
@@ -18,6 +18,7 @@ const LeftPanel = ({
   const [openMenu, setopenMenu] = useState(false);
   const navigate = useNavigate();
   const [channelData, setchannelData] = useState(blankChannelData);
+  const [isSlide, setisSlide] = useState(false);
 
   const getChannelData = async (channelName) => {
     const data = await api.getChannelData(channelName);
@@ -25,9 +26,10 @@ const LeftPanel = ({
     if (data.status === "OK") setchannelData((channelData) => data.channelData);
   };
 
-  useEffect(() => {
-    getChannelData(channelName);
-  }, [channelName]);
+  const slide = async (channelName) => {
+    await getChannelData(channelName);
+    setisSlide(true);
+  };
 
   const uploadProfilePic = async (event) => {
     if (event.target.files.length > 0) {
@@ -50,33 +52,93 @@ const LeftPanel = ({
   };
 
   return (
-    <div className="channel-list-container w-1/4 h-full bg-black flex flex-col justify-between">
-      <div className="channel-list-header flex items-center justify-between h-16 px-8 font-bold text-white shadow-md">
-        Channels
-        <div
-          className="h-6 w-6 border-2 border-white flex justify-center items-center rounded-sm cursor-pointer hover:bg-gray-700"
-          onClick={() => setopenAddChannel(true)}
-        >
-          <span className="material-icons text-white text-base">add</span>
+    <div className="channel-list-container w-1/4 h-full bg-black flex flex-col justify-between overflow-hidden">
+      <section
+        className={
+          "main-section h-full relative transition-transform ease-linear " +
+          (isSlide ? "-translate-x-full" : null)
+        }
+      >
+        <div className="channel-list-header flex items-center justify-between h-16 px-8 font-bold text-white shadow-md">
+          Channels
+          <div
+            className="h-6 w-6 border-2 border-white flex justify-center items-center rounded-sm cursor-pointer hover:bg-gray-700"
+            onClick={() => setopenAddChannel(true)}
+          >
+            <span className="material-icons text-white text-base">add</span>
+          </div>
         </div>
-      </div>
-      <div className="channel-list text-white text-sm p-8 w-full h-full overflow-x-hidden whitespace-nowrap text-ellipsis">
-        {userData.channelList.map((channel) => {
-          const link = `/channel/${channel}`;
-          return (
-            <Link to={link} key={Math.random(1000)}>
-              <h1
-                className={
-                  "uppercase font-bold cursor-pointer w-full p-4 hover:bg-gray-800 " +
-                  (channelName === channel ? "bg-gray-800" : null)
-                }
-              >
-                {channel}
-              </h1>
-            </Link>
-          );
-        })}
-      </div>
+        <div className="channel-list text-white text-sm p-8 w-full h-full overflow-x-hidden whitespace-nowrap text-ellipsis">
+          {userData.channelList.map((channel) => {
+            const link = `/channel/${channel}`;
+            return (
+              <div key={Math.random(1000)} className="relative">
+                <Link to={link}>
+                  <h1
+                    className={
+                      "uppercase font-bold cursor-pointer w-full p-4 hover:bg-gray-800 " +
+                      (channelName === channel ? "bg-gray-800" : null)
+                    }
+                  >
+                    {channel}
+                  </h1>
+                </Link>
+                <div>
+                  <span
+                    className="material-icons absolute top-0 h-full w-12 flex justify-center items-center cursor-pointer hover:bg-gray-700 right-0"
+                    onClick={() => slide(channel)}
+                  >
+                    navigate_next
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <section className="slide-section absolute left-full top-0 w-full">
+          <div className="channel-list-header flex items-center justify-between h-16 px-8 font-bold text-white shadow-md w-full relative">
+            {channelData.channelName}
+            <span
+              className="material-icons absolute right-8 cursor-pointer"
+              onClick={() => setisSlide(false)}
+            >
+              close
+            </span>
+          </div>
+          <div className="w-full h-40 p-8 text-white">
+            <h1 className="uppercase text-lg font-bold mb-4 ">Description</h1>
+            <div className="h-full overflow-hidden text-ellipsis">
+              {channelData.desc.length === 0 ? (
+                <div>No description</div>
+              ) : (
+                <>{channelData.desc}</>
+              )}
+            </div>
+          </div>
+          <div className="channel-list text-white text-sm p-8 w-full h-full overflow-x-hidden whitespace-nowrap text-ellipsis">
+            <h1 className="uppercase text-lg font-bold mb-4">Members</h1>
+            <div className="overflow-y-auto h-96">
+              {channelData.members.length === 0 ? (
+                <h1>There are no members.</h1>
+              ) : (
+                <>
+                  {channelData.members.map((member) => {
+                    return (
+                      <h1
+                        className="uppercase font-bold w-full"
+                        key={Math.random(2000)}
+                      >
+                        {member}
+                      </h1>
+                    );
+                  })}
+                </>
+              )}
+            </div>
+          </div>
+        </section>
+      </section>
+
       <div className="relative text-white">
         <div
           className={
